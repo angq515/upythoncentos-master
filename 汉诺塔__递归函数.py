0170f70515b5c2d789b9887604d51c6c76268d21
angq515 <571471156@qq.com>
2019-02-12 11:19:11 +1100

    initial commit

--- 汉诺塔__递归函数.py
+++ 汉诺塔__递归函数.py
@@ -1,21 +1,26 @@
-# _*_ codiong: utf-8 -*-
+# -*- coding: utf-8 -*-
 
-# n! = 1*2*3*...*n
-# 我的方法：
-# def fact(n):
-#     x = 1
-#     i = n
-#     while i > 0:
-#         x = x*(n-i+1)
-#         i -= 1
-#     return x
-#
-# print(fact(5))
+# 请编写hanoi(n, a, b, c)函数，它接收参数n，
+# 表示3个柱子A、B、C中第1个柱子A的盘子数量，
+# 然后打印出把所有盘子从A借助B移动到C的方法，例如：
 
-
-def fact(n):
+def hanoi(n, a, b, c):
     if n == 1:
-        return 1
-    return n*fact(n-1)
+        print(a, '-->', c)
+    else:
+        hanoi(n - 1, a, c, b)
+        print(a, '-->', c)
+        hanoi(n - 1, b, a, c)
+# 调用
+hanoi(1, 'A', 'B', 'C')
 
-print(fact(999))
+
+# 期待输出:
+# A --> C
+# A --> B
+# C --> B
+# A --> C
+# B --> A
+# B --> C
+# A --> C
+move(3, 'A', 'B', 'C')
